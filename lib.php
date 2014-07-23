13fbc9c30b672c8daa487df59e3c3a8397bb6a33
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-07-23 12:03:34 +0100

    Cleanup

--- lib.php
+++ lib.php
@@ -255,13 +255,12 @@ class enrol_connect_plugin extends enrol_plugin
                 $user = $enrolment->user;
                 $role = $enrolment->role;
 
-                // Try to create the user.
+                // Try to create the user if it does not exist.
                 if (!$user->is_in_moodle()) {
                     $user->create_in_moodle();
                 }
 
-                // Check these things are in Moodle,
-                // skip if not (this plugin does not create new users/roles).
+                // Check these things are in Moodle.
                 if (empty($user->mid) || empty($role->mid)) {
                     continue;
                 }
